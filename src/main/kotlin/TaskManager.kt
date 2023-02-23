17aac28b30922c3fcae60e69e180e83dbb5aa8e5
Stepyko Tatyana <SunnyTanya93@mail.ru>
2023-02-23 15:42:25 +0700

    Task project and just practice with difference tasks

--- src/main/kotlin/TaskManager.kt
+++ src/main/kotlin/TaskManager.kt
@@ -1,19 +1,111 @@
+import java.lang.Exception
+import kotlin.math.max
+
 fun main() {
     val mapWithInput = mutableMapOf<Int, String>()
-    var i = 1;
+    var i = 1
     while (true) {
-        val input = readln()
-        if (input == "") {
-            break
-        } else {
-            mapWithInput[i] = input.trim()
-            i++;
+        println("Input an action (add, print, end):")
+        when (readln()) {
+            "add" -> {
+                println("Input a new task (enter a blank line to end):")
+                var taskList = ""
+                var j = 1
+                while (true) {
+                    val input = readln().trim()
+                    if (input == "") {
+                        break
+                    } else {
+                        if (j == 1) {
+                            taskList += "$i\t" + input + "\n"
+                            j++
+                        } else {
+                            taskList += "\t" + input + "\n"
+                        }
+
+
+                    }
+                }
+                if (taskList != "") {
+                    mapWithInput[i] = taskList
+                    i++
+                } else {
+                    println("The task is blank")
+                }
+
+            }
+
+
+            "print" -> {
+                if (mapWithInput.isNotEmpty()) {
+                    for (str in mapWithInput) {
+                        //print(str.key)
+                        println(str.value)
+                    }
+                } else {
+                    println("No task have been input")
+                }
+            }
+
+            "end" -> {
+                println("Tasklist exiting!")
+                break
+            }
+
+            else -> {
+                println("The input action is invalid")
+            }
         }
     }
-    if (mapWithInput.isNotEmpty()) {
-        for (str in mapWithInput) println("${str.key}  ${str.value}")
-    } else {
-        println("No task have been input")
+
+
+}
+
+fun arrayTask() {
+    //    1 вариант
+    val n = readln().toInt()
+    val list = IntArray(n)
+    for (i in 0 until n) {
+        list[i] = readln().toInt()
     }
+
+//    2 вариант
+
+    val listArray = Array(readln().toInt()) { readln().toInt() }
+
+
+    val m = readln().toInt()
+    println(list.filter { x -> x == m }.size)
+}
+
+fun arrayTask2() {
+    val n = readln().toInt()
+    val listCompany = Array(n) { readln().toInt() }
+
+    val list = readln().split(" ")
+    val p = list[0].toInt()
+    val m = list[1].toInt()
+    var flag = false
+    for (i in listCompany.indices) {
+        if (listCompany[i] == p || listCompany[i] == m) {
+            flag = if (listCompany[i] == p)
+                try {
+                    listCompany[i + 1] == m
+
+                } catch (e: Exception) {
+                    false
+                }
+            else {
+                try {
+                    listCompany[i + 1] == p
+                } catch (e: Exception) {
+                    false
+                }
+            }
+            if (flag) break
+        }
+    }
+    if (flag) println("NO") else println("YES")
+
 
 }
